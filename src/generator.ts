6846d3870524164d582986827d52737e81667aab
Andre Restivo <andre.restivo@gmail.com>
2018-12-27 16:50:34 +0000

    Default assets folder and comments

--- src/generator.ts
+++ src/generator.ts
@@ -6,9 +6,7 @@ import fs from 'fs'
 import yaml from 'js-yaml'
 
 import MarkdownIt from 'markdown-it'
-
-import minifyCSS from 'clean-css'
-import minifyJS from 'babel-minify'
+import markdownItAttrs from 'markdown-it-attrs';
 
 import rmdir from 'rimraf'
 import ncp from 'ncp'
@@ -20,12 +18,13 @@ interface Slide {
 }
 
 const markdownIt = new MarkdownIt()
+markdownIt.use(markdownItAttrs)
 
 program
   .version('0.0.1')
   .arguments('<markdown>')
   .option('-o, --output <dir>', 'Output directory', 'output')
-  .option('-a, --assets <dir>', 'Assets directory')
+  .option('-a, --assets <dir>', 'Assets directory (default: "assets/<name>")')
   .option('-t, --theme <name>', 'Theme name', 'default')
   .parse(process.argv)
 
@@ -36,26 +35,31 @@ const output = program.output
 const theme = program.theme
 const assets = program.assets
 
+// Verify markdown parameter
 if (input === undefined) {
   console.error('No markdown file specified!')
   process.exit(1)
 }
 
+// Verify if markdown file exists
 if (!fs.existsSync(input)) {
   console.error('Markdown file not found!')
   process.exit(1)
 }
 
+// Verify if theme exists
 if (!fs.existsSync(`themes/${theme}`)) {
   console.error('Theme not found!')
   process.exit(1)
 }
 
+// Verify if assets folder exists
 if (assets !== undefined && !fs.existsSync(`${assets}`)) {
   console.error('Assets not found!')
   process.exit(1)
 }
 
+// Build the slides
 build(input, output, theme, assets)
 
 /**
@@ -73,9 +77,9 @@ function build(input: string, output: string, theme: string, assets: string) {
 
   render(slides, frontmatter, output, name)
 
+  copyLecture(output)
   copyTheme(theme, output)
-  if (assets !== undefined)
-    copyAssets(assets, output, name)
+  copyAssets(assets, output, name)
 }
 
 /**
@@ -123,9 +127,7 @@ function render(slides: Slide[], frontmatter: string, output: string, name: stri
   let contents = ''
   contents += mustache.render(header, {
     theme,
-    show: yaml.load(frontmatter),
-    'lecture-css': new minifyCSS({}).minify(readFile('templates/lecture.css')).styles,
-    'lecture-script': minifyJS(readFile('templates/lecture.js'), {}, {}).code
+    show: yaml.load(frontmatter)
   })
 
   let num = 1
@@ -148,6 +150,18 @@ function render(slides: Slide[], frontmatter: string, output: string, name: stri
 }
 
 /**
+ * Copies CSS and JS base files to output folders
+ * @param output The output folder
+ */
+function copyLecture(output: string) {
+  if (!fs.existsSync(output)) fs.mkdirSync(output)
+  if (!fs.existsSync(`${output}/lecture.js`)) fs.mkdirSync(`${output}/lecture.js`)
+
+  fs.copyFileSync('templates/lecture.css', `${output}/lecture.js/lecture.css`)
+  fs.copyFileSync('templates/lecture.js', `${output}/lecture.js/lecture.js`)
+}
+
+/**
  * Copies a theme to the output directory
  * @param theme The name of the theme to copy.
  * @param output Output directory
@@ -161,13 +175,24 @@ function copyTheme(theme : string, output : string) {
   ncp.ncp(`themes/${theme}`, `${output}/themes/${theme}`, _ => {})
 }
 
+/**
+ * Copies assets to the output folder. If assets are undefined tries
+ * to find assets in default folder (assets/name).
+ * @param assets The assets folder
+ * @param output The output folder
+ * @param name The markdown name
+ */
 function copyAssets(assets: string, output: string, name: string) {
-  if (!fs.existsSync(output)) fs.mkdirSync(output)
-  if (!fs.existsSync(`${output}/assets`)) fs.mkdirSync(`${output}/assets`)
-  if (fs.existsSync(`${output}/assets/${name}`))
-    rmdir.sync(`${output}/assets/${name}`)
+  if (assets === undefined && fs.existsSync(`assets/${name}`)) {
+    copyAssets(`assets/${name}`, output, name)
+  } else {
+    if (!fs.existsSync(output)) fs.mkdirSync(output)
+    if (!fs.existsSync(`${output}/assets`)) fs.mkdirSync(`${output}/assets`)
+    if (fs.existsSync(`${output}/assets/${name}`))
+      rmdir.sync(`${output}/assets/${name}`)
 
-  ncp.ncp(`${assets}`, `${output}/assets/${name}`, _ => {})
+    ncp.ncp(`${assets}`, `${output}/assets/${name}`, _ => {})
+  }
 }
 
 /**
@@ -187,6 +212,10 @@ function* split(contents: string) {
   yield(part)
 }
 
+/**
+ * Helper function that reads a file asynchronously in UTF-8.
+ * @param path The file to be read.
+ */
 function readFile(path: string) {
   return fs.readFileSync(path, 'utf8')
 }
